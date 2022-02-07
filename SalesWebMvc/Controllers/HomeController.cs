a15efac79624d3efc790cefbd0dd47fdfad7f25b
Kaique <kaiquevan@hotmail.com>
2022-02-06 22:57:51 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
+            ViewData["Professor"] = "Nelio Alves";
 
             return View();
         }
